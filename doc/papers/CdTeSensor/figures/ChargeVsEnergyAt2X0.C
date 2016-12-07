b8d2648d9ebb8151095f82f66647d0192ded2d67
SiXie <Si.Xie@cern.ch>
2016-12-07 19:48:03 +0100

    update T9 energy plots

--- doc/papers/CdTeSensor/figures/ChargeVsEnergyAt2X0.C
+++ doc/papers/CdTeSensor/figures/ChargeVsEnergyAt2X0.C
@@ -1,16 +1,41 @@
+void ChargeVsEnergyAt2X0()
 {
 //=========Macro generated from canvas: c/c
-//=========  (Thu Dec  1 17:02:25 2016) by ROOT version5.34/36
+//=========  (Wed Dec  7 19:33:49 2016) by ROOT version6.02/05
    TCanvas *c = new TCanvas("c", "c",2,77,800,800);
    c->Range(0,0,1,1);
    c->SetFillColor(0);
    c->SetBorderMode(0);
    c->SetBorderSize(2);
-   c->SetLeftMargin(0.15);
+   c->SetLeftMargin(0.17);
    c->SetBottomMargin(0.12);
    c->SetFrameBorderMode(0);
    
-   TGraphErrors *gre = new TGraphErrors(4);
+   Double_t Graph0_fx1002[5] = {
+   0,
+   2,
+   3.5,
+   5,
+   7};
+   Double_t Graph0_fy1002[5] = {
+   -100,
+   0.076,
+   0.104,
+   0.161,
+   0.243};
+   Double_t Graph0_fex1002[5] = {
+   0,
+   0,
+   0,
+   0,
+   0};
+   Double_t Graph0_fey1002[5] = {
+   0,
+   0.062,
+   0.07,
+   0.094,
+   0.118};
+   TGraphErrors *gre = new TGraphErrors(5,Graph0_fx1002,Graph0_fy1002,Graph0_fex1002,Graph0_fey1002);
    gre->SetName("Graph0");
    gre->SetTitle("");
    gre->SetFillColor(1);
@@ -21,44 +46,61 @@
    gre->SetLineColor(ci);
    gre->SetLineWidth(3);
    gre->SetMarkerStyle(20);
-   gre->SetPoint(0,2,0.16);
-   gre->SetPointError(0,0,0.08);
-   gre->SetPoint(1,3.5,0.26);
-   gre->SetPointError(1,0,0.15);
-   gre->SetPoint(2,5,0.26);
-   gre->SetPointError(2,0,0.17);
-   gre->SetPoint(3,7,0.4);
-   gre->SetPointError(3,0,0.2);
+   gre->SetMarkerSize(1.25);
    
-   TH1F *Graph_Graph2 = new TH1F("Graph_Graph2","",100,1.5,7.5);
-   Graph_Graph2->SetMinimum(0);
-   Graph_Graph2->SetMaximum(0.75);
-   Graph_Graph2->SetDirectory(0);
-   Graph_Graph2->SetStats(0);
+   TH1F *Graph_Graph1002 = new TH1F("Graph_Graph1002","",100,0,7.7);
+   Graph_Graph1002->SetMinimum(0);
+   Graph_Graph1002->SetMaximum(0.4);
+   Graph_Graph1002->SetDirectory(0);
+   Graph_Graph1002->SetStats(0);
 
    ci = TColor::GetColor("#000099");
-   Graph_Graph2->SetLineColor(ci);
-   Graph_Graph2->GetXaxis()->SetTitle("Electron Beam Energy [GeV]");
-   Graph_Graph2->GetXaxis()->SetLabelFont(42);
-   Graph_Graph2->GetXaxis()->SetLabelSize(0.045);
-   Graph_Graph2->GetXaxis()->SetTitleSize(0.045);
-   Graph_Graph2->GetXaxis()->SetTitleOffset(1.05);
-   Graph_Graph2->GetXaxis()->SetTitleFont(42);
-   Graph_Graph2->GetYaxis()->SetTitle("Integrated Charge [pC]");
-   Graph_Graph2->GetYaxis()->SetLabelFont(42);
-   Graph_Graph2->GetYaxis()->SetLabelSize(0.045);
-   Graph_Graph2->GetYaxis()->SetTitleSize(0.05);
-   Graph_Graph2->GetYaxis()->SetTitleOffset(1.2);
-   Graph_Graph2->GetYaxis()->SetTitleFont(42);
-   Graph_Graph2->GetZaxis()->SetLabelFont(42);
-   Graph_Graph2->GetZaxis()->SetLabelSize(0.035);
-   Graph_Graph2->GetZaxis()->SetTitleSize(0.035);
-   Graph_Graph2->GetZaxis()->SetTitleFont(42);
-   gre->SetHistogram(Graph_Graph2);
+   Graph_Graph1002->SetLineColor(ci);
+   Graph_Graph1002->GetXaxis()->SetTitle("Electron Beam Energy [GeV]");
+   Graph_Graph1002->GetXaxis()->SetLabelFont(42);
+   Graph_Graph1002->GetXaxis()->SetLabelSize(0.045);
+   Graph_Graph1002->GetXaxis()->SetTitleSize(0.045);
+   Graph_Graph1002->GetXaxis()->SetTitleOffset(1.05);
+   Graph_Graph1002->GetXaxis()->SetTitleFont(42);
+   Graph_Graph1002->GetYaxis()->SetTitle("Integrated Charge [pC]");
+   Graph_Graph1002->GetYaxis()->SetLabelFont(42);
+   Graph_Graph1002->GetYaxis()->SetLabelSize(0.045);
+   Graph_Graph1002->GetYaxis()->SetTitleSize(0.05);
+   Graph_Graph1002->GetYaxis()->SetTitleOffset(1.5);
+   Graph_Graph1002->GetYaxis()->SetTitleFont(42);
+   Graph_Graph1002->GetZaxis()->SetLabelFont(42);
+   Graph_Graph1002->GetZaxis()->SetLabelSize(0.035);
+   Graph_Graph1002->GetZaxis()->SetTitleSize(0.035);
+   Graph_Graph1002->GetZaxis()->SetTitleFont(42);
+   gre->SetHistogram(Graph_Graph1002);
    
    gre->Draw("ap");
    
-   gre = new TGraphErrors(4);
+   Double_t Graph1_fx1003[5] = {
+   0,
+   2,
+   3.5,
+   5,
+   7};
+   Double_t Graph1_fy1003[5] = {
+   -100,
+   0.076,
+   0.104,
+   0.161,
+   0.243};
+   Double_t Graph1_fex1003[5] = {
+   0,
+   0,
+   0,
+   0,
+   0};
+   Double_t Graph1_fey1003[5] = {
+   0,
+   0.006,
+   0.003,
+   0.005,
+   0.008};
+   gre = new TGraphErrors(5,Graph1_fx1003,Graph1_fy1003,Graph1_fex1003,Graph1_fey1003);
    gre->SetName("Graph1");
    gre->SetTitle("Graph");
    gre->SetFillColor(1);
@@ -66,75 +108,12 @@
    ci = TColor::GetColor("#0000ff");
    gre->SetLineColor(ci);
    gre->SetLineWidth(2);
-   gre->SetPoint(0,2,0.16);
-   gre->SetPointError(0,0,0.008);
-   gre->SetPoint(1,3.5,0.26);
-   gre->SetPointError(1,0,0.015);
-   gre->SetPoint(2,5,0.26);
-   gre->SetPointError(2,0,0.03);
-   gre->SetPoint(3,7,0.4);
-   gre->SetPointError(3,0,0.026);
-   
-   TH1F *Graph_Graph3 = new TH1F("Graph_Graph3","Graph",100,1.5,7.5);
-   Graph_Graph3->SetMinimum(0.1246);
-   Graph_Graph3->SetMaximum(0.4534);
-   Graph_Graph3->SetDirectory(0);
-   Graph_Graph3->SetStats(0);
-
-   ci = TColor::GetColor("#000099");
-   Graph_Graph3->SetLineColor(ci);
-   Graph_Graph3->GetXaxis()->SetLabelFont(42);
-   Graph_Graph3->GetXaxis()->SetLabelSize(0.035);
-   Graph_Graph3->GetXaxis()->SetTitleSize(0.035);
-   Graph_Graph3->GetXaxis()->SetTitleFont(42);
-   Graph_Graph3->GetYaxis()->SetLabelFont(42);
-   Graph_Graph3->GetYaxis()->SetLabelSize(0.035);
-   Graph_Graph3->GetYaxis()->SetTitleSize(0.035);
-   Graph_Graph3->GetYaxis()->SetTitleFont(42);
-   Graph_Graph3->GetZaxis()->SetLabelFont(42);
-   Graph_Graph3->GetZaxis()->SetLabelSize(0.035);
-   Graph_Graph3->GetZaxis()->SetTitleSize(0.035);
-   Graph_Graph3->GetZaxis()->SetTitleFont(42);
-   gre->SetHistogram(Graph_Graph3);
-   
-   
-   TF1 *pol1 = new TF1("pol1","pol1",1.5,7.5);
-   pol1->SetFillColor(19);
-   pol1->SetFillStyle(0);
-   pol1->SetLineColor(2);
-   pol1->SetLineWidth(2);
-   pol1->SetChisquare(5.376196);
-   pol1->SetNDF(2);
-   pol1->GetXaxis()->SetLabelFont(42);
-   pol1->GetXaxis()->SetLabelSize(0.035);
-   pol1->GetXaxis()->SetTitleSize(0.035);
-   pol1->GetXaxis()->SetTitleFont(42);
-   pol1->GetYaxis()->SetLabelFont(42);
-   pol1->GetYaxis()->SetLabelSize(0.035);
-   pol1->GetYaxis()->SetTitleSize(0.035);
-   pol1->GetYaxis()->SetTitleFont(42);
-   pol1->SetParameter(0,0.06847663);
-   pol1->SetParError(0,0.0148229);
-   pol1->SetParLimits(0,0,0);
-   pol1->SetParameter(1,0.04758728);
-   pol1->SetParError(1,0.00478767);
-   pol1->SetParLimits(1,0,0);
-   gre->GetListOfFunctions()->Add(pol1);
    gre->Draw("pe1");
    TLatex *   tex = new TLatex(0.35,0.93,"Absorber : 2 X_{0} Lead");
 tex->SetNDC();
    tex->SetTextFont(42);
    tex->SetLineWidth(2);
    tex->Draw();
-   
-   TPaveText *pt = new TPaveText(0.442381,0.9346907,0.557619,0.995,"blNDC");
-   pt->SetName("title");
-   pt->SetBorderSize(0);
-   pt->SetFillColor(0);
-   pt->SetFillStyle(0);
-   pt->SetTextFont(42);
-   text = pt->AddText("pol1");
-   pt->Draw();
    c->Modified();
    c->cd();
    c->SetSelected(c);
--- doc/papers/CdTeSensor/figures/ChargeVsEnergyAt6X0.C
+++ doc/papers/CdTeSensor/figures/ChargeVsEnergyAt6X0.C
@@ -1,8 +1,8 @@
 void ChargeVsEnergyAt6X0()
 {
 //=========Macro generated from canvas: c/c
-//=========  (Tue Dec  6 17:50:10 2016) by ROOT version6.02/05
-   TCanvas *c = new TCanvas("c", "c",2,51,800,800);
+//=========  (Wed Dec  7 19:33:48 2016) by ROOT version6.02/05
+   TCanvas *c = new TCanvas("c", "c",4,77,800,800);
    c->Range(0,0,1,1);
    c->SetFillColor(0);
    c->SetBorderMode(0);
@@ -11,23 +11,23 @@ void ChargeVsEnergyAt6X0()
    c->SetBottomMargin(0.12);
    c->SetFrameBorderMode(0);
    
-   Double_t Graph0_fx1002[3] = {
+   Double_t Graph0_fx1001[3] = {
    50,
    100,
    200};
-   Double_t Graph0_fy1002[3] = {
+   Double_t Graph0_fy1001[3] = {
    3.6,
    6.8,
    11.5};
-   Double_t Graph0_fex1002[3] = {
+   Double_t Graph0_fex1001[3] = {
    0,
    0,
    0};
-   Double_t Graph0_fey1002[3] = {
+   Double_t Graph0_fey1001[3] = {
    0.6,
    1.2,
    2.2};
-   TGraphErrors *gre = new TGraphErrors(3,Graph0_fx1002,Graph0_fy1002,Graph0_fex1002,Graph0_fey1002);
+   TGraphErrors *gre = new TGraphErrors(3,Graph0_fx1001,Graph0_fy1001,Graph0_fex1001,Graph0_fey1001);
    gre->SetName("Graph0");
    gre->SetTitle("");
    gre->SetFillColor(1);
@@ -38,32 +38,33 @@ void ChargeVsEnergyAt6X0()
    gre->SetLineColor(ci);
    gre->SetLineWidth(2);
    gre->SetMarkerStyle(20);
+   gre->SetMarkerSize(1.25);
    
-   TH1F *Graph_Graph1002 = new TH1F("Graph_Graph1002","",100,35,215);
-   Graph_Graph1002->SetMinimum(0);
-   Graph_Graph1002->SetMaximum(15);
-   Graph_Graph1002->SetDirectory(0);
-   Graph_Graph1002->SetStats(0);
+   TH1F *Graph_Graph1001 = new TH1F("Graph_Graph1001","",100,35,215);
+   Graph_Graph1001->SetMinimum(0);
+   Graph_Graph1001->SetMaximum(15);
+   Graph_Graph1001->SetDirectory(0);
+   Graph_Graph1001->SetStats(0);
 
    ci = TColor::GetColor("#000099");
-   Graph_Graph1002->SetLineColor(ci);
-   Graph_Graph1002->GetXaxis()->SetTitle("Electron Beam Energy [GeV]");
-   Graph_Graph1002->GetXaxis()->SetLabelFont(42);
-   Graph_Graph1002->GetXaxis()->SetLabelSize(0.045);
-   Graph_Graph1002->GetXaxis()->SetTitleSize(0.045);
-   Graph_Graph1002->GetXaxis()->SetTitleOffset(1.05);
-   Graph_Graph1002->GetXaxis()->SetTitleFont(42);
-   Graph_Graph1002->GetYaxis()->SetTitle("Integrated Charge [pC]");
-   Graph_Graph1002->GetYaxis()->SetLabelFont(42);
-   Graph_Graph1002->GetYaxis()->SetLabelSize(0.045);
-   Graph_Graph1002->GetYaxis()->SetTitleSize(0.05);
-   Graph_Graph1002->GetYaxis()->SetTitleOffset(1.2);
-   Graph_Graph1002->GetYaxis()->SetTitleFont(42);
-   Graph_Graph1002->GetZaxis()->SetLabelFont(42);
-   Graph_Graph1002->GetZaxis()->SetLabelSize(0.035);
-   Graph_Graph1002->GetZaxis()->SetTitleSize(0.035);
-   Graph_Graph1002->GetZaxis()->SetTitleFont(42);
-   gre->SetHistogram(Graph_Graph1002);
+   Graph_Graph1001->SetLineColor(ci);
+   Graph_Graph1001->GetXaxis()->SetTitle("Electron Beam Energy [GeV]");
+   Graph_Graph1001->GetXaxis()->SetLabelFont(42);
+   Graph_Graph1001->GetXaxis()->SetLabelSize(0.045);
+   Graph_Graph1001->GetXaxis()->SetTitleSize(0.045);
+   Graph_Graph1001->GetXaxis()->SetTitleOffset(1.05);
+   Graph_Graph1001->GetXaxis()->SetTitleFont(42);
+   Graph_Graph1001->GetYaxis()->SetTitle("Integrated Charge [pC]");
+   Graph_Graph1001->GetYaxis()->SetLabelFont(42);
+   Graph_Graph1001->GetYaxis()->SetLabelSize(0.045);
+   Graph_Graph1001->GetYaxis()->SetTitleSize(0.05);
+   Graph_Graph1001->GetYaxis()->SetTitleOffset(1.2);
+   Graph_Graph1001->GetYaxis()->SetTitleFont(42);
+   Graph_Graph1001->GetZaxis()->SetLabelFont(42);
+   Graph_Graph1001->GetZaxis()->SetLabelSize(0.035);
+   Graph_Graph1001->GetZaxis()->SetTitleSize(0.035);
+   Graph_Graph1001->GetZaxis()->SetTitleFont(42);
+   gre->SetHistogram(Graph_Graph1001);
    
    gre->Draw("ap");
    TLatex *   tex = new TLatex(0.3,0.93,"Absorber : 6 X_{0} W-Pb");
